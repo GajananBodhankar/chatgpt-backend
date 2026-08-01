585e3127edbbae9065eb8fd74c104df6ea1a70a8
GajananBodhankar <bodhankargajanan99@gmail.com>
2026-08-01 21:39:06 +0530

    Daily commit

--- src/main/java/com/chapt_gpt_clone/chaptgpt/security/JwtService.java
+++ src/main/java/com/chapt_gpt_clone/chaptgpt/security/JwtService.java
@@ -50,6 +50,7 @@ public class JwtService {
     }
 
     private Claims extractClaims(String token) {
+
         return Jwts.parser()
                 .verifyWith(getSigningKey())
                 .build()
